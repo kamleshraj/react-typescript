c0cbf90452b19a2769070c7a411b7bffc3718902
kamleshraj <kamleshk.uiuxdesigner@gmail.com>
2023-04-27 16:26:48 +0530

    testing git on main

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -62,6 +62,7 @@ const UserDetails:React.FC=()=>{
             </table>
             <Link to='/'>Back To Home</Link>
             <p>this is for git</p>
+            <h3>I am in the main brach do not replace</h3>
         </>
     )
 }
